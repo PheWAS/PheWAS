967a0477b58d03788a7482cc06141df751ea7735
RobertJCarroll <Robert.J.Carroll@vanderbilt.edu>
2014-12-05 17:08:40 -0600

    0.9.5: Update to use dplyr and tidyr
    createPhewasTable now uses faster methods to transform the data.

--- R/mapICD9toPheWAS.R
+++ R/mapICD9toPheWAS.R
@@ -10,9 +10,10 @@ function(..., icd9s, add.info=F, keep.icd9=F) {
     stop("Must supply a data frame with an 'icd9' column or specify the icd9s parameter")
   }
   if(class(input$icd9) %in% c("integer","numeric")) {stop("Numeric ICD-9 codes passed in, so an accurate mapping is not possible. E.G.: 250, 250.0, and 250.00 are different codes and necessitate string representation")}
-  output = merge(input,phemap)
+  #merge the tables
+  output = inner_join(input,phemap,by="icd9")
   if(add.info){
-    output=merge(output,pheinfo)
+    output=inner_join(output,pheinfo,by="phewas_code")
   }
   if(!keep.icd9) {
     output=subset(output, select = -icd9)
