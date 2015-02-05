7b0d2f8182b7bb335542ad6d6f258ee037e7facd
RobertJCarroll <Robert.J.Carroll@vanderbilt.edu>
2015-02-05 17:26:43 -0600

    Various testing changes for exclusion mapping

--- R/mapPheWAStoExclusions.R
+++ R/mapPheWAStoExclusions.R
@@ -1,12 +1,31 @@
 mapPheWAStoExclusions <-
-  function(phewas.codes, ids=NA) {
-    if(is.na(ids)[1]) {
-      input = data.frame(exclusion_criteria=phewas.codes)
+  function(phewas.codes, ids) {
+    if(missing(ids)) {
+      input = data.frame(id=0,exclusion_criteria=phewas.codes,stringsAsFactors = FALSE)
     }
     else {
-      input = data.frame(id=ids, exclusion_criteria=phewas.codes)
+      input = data.frame(id=ids, exclusion_criteria=phewas.codes,stringsAsFactors = FALSE)
     }
-    output = inner_join(input,phewas_exclude,by="exclusion_criteria")
-    output = output %>% transmute(id,phe=code) %>% distinct()
+#    output = rbind_all(by(input,input$id,function(by_id){
+#      codes=unique(unlist(lapply(ex_split,function(by_range) {
+#        match=max(by_id$exclusion_criteria>=by_range$low&
+#              #by_id$exclusion_criteria>=by_range$lownum&
+#              by_id$exclusion_criteria<=by_range$high#&
+#              #by_id$exclusion_criteria<=by_range$highnum
+#              )
+#        if(match) {by_range$codes}
+#        else {c()}
+#      })))
+#      data.frame(id=by_id$id[1],phe=unique(codes))
+#    }))
+    #output = output %>% transmute(id,phe=code) %>% distinct()
+    exc_groups = rbind_all(by(input,input$id,function(by_id){
+       merge(by_id,ex_df,by=c()) %>%
+        filter(exclusion_criteria>=low,exclusion_criteria<=high) %>%
+        dplyr::select(id,low,high) %>% distinct()
+    }))
+
+    output = inner_join(exc_groups,ex_reps) %>%
+      dplyr::select(id,phe) %>% distinct()
     output
   }
