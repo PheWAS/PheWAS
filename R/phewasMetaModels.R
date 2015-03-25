5bb59cdcff849e26dd5878ef086f0a1b3286f232
RobertJCarroll <Robert.J.Carroll@vanderbilt.edu>
2015-03-25 16:10:31 -0500

    Update to prepare for CRAN submission
    Added tweaks to documentation and more to enable passing the package
    check.

--- R/phewasMetaModels.R
+++ R/phewasMetaModels.R
@@ -9,23 +9,28 @@ phewasMetaModels <- function(results, ...) {
            snp=a$snp[1]
            adjustment=a$adjustment[1]
            title=paste0(phenotype," ",snp," ",adjustment)
-           type=suppressWarnings(max(as.character(a$type),na.rm=TRUE))
-           #If type is -Inf, no succesful anaylses exist in this subset
-           if(type==-Inf) { data.frame(note="Error: No succesful analysis in this subset",title=title) }
-           else { #Otherwise, perform the analysis
-           #Throw an error if study types, e.g. logistic and linear, don't match
-           if(length(unique(a$type))!=1) stop(paste0("Study types do not match for ",phenotype,", ",snp,", ",adjustment,"."))
-           sm=""
-           #Define the Odds Ratio summary measure if appropriate
-           if(type=="logistic") {
-             sm="OR"
-           } 
-           #Warn if the type is not linear or logistic
-           if(!type %in% c("logistic","linear")) {
-             warning(paste0("No match for study type ",type,". Assuming no summary measure."))
-           }
-           metagen(TE=a$beta,seTE=a$SE, sm=sm,n.e=a$n_cases,n.c=a$n_controls,
-                   studlab=a$study, title=title,...)}})
+           #Remove NA results from the input
+           a=a[!is.na(a$p),]
+           #If there are no records left, return a data frame with a note (so one still knows it was there)
+           if(nrow(a)==0) {
+             data.frame(note="Error: No succesful analysis in this subset",title=title) 
+           } else {#If there was at least one good analysis, calculate the meta-analysis
+             #Define the Odds Ratio summary measure if appropriate
+             sm=""
+             type=a$type[1]
+             if(type=="logistic") {
+               sm="OR"
+             } 
+             #Warn if the type is not linear or logistic
+             if(!type %in% c("logistic","linear")) {
+               warning(paste0("No match for study type ",type,". Assuming no summary measure for metagen."))
+             }
+             #Throw an error if study types, e.g. logistic and linear, don't match
+             if(length(na.omit(unique(a$type)))>1) stop(paste0("Study types do not match for ",phenotype,", ",snp,", ",adjustment,"."))
+             #Perform the meta analysis.
+             metagen(TE=a$beta,seTE=a$SE, sm=sm,n.e=a$n_cases,n.c=a$n_controls,
+                       studlab=a$study, title=paste0(phenotype," ",snp," ",adjustment), ...)
+           }})
   names(out)=sapply(out,function(x){x$title})
   out
 }
