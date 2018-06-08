0a540ab2b889f0dd71ae5b7eb79dd8cd319d422e
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2018-06-08 09:57:08 -0500

    Add multidplyr support for phewasmeta

--- R/phewasMeta.R
+++ R/phewasMeta.R
@@ -1,6 +1,18 @@
-phewasMeta <- function(results, fixed=T, keep.both=T, ...) {
+phewasMeta <- function(results, fixed=T, keep.both=T, cores=1, ...) {
   #Replace NA adjustment values with "NA"- necessary for by
-  results$adjustment = ifelse(is.na(results$adjustment),"NA",results$adjustment)
-  #Iterate across all phenotype, snp, adjustment combinations.
-  out = results %>% group_by(phenotype,snp,adjustment) %>% do(phewas_meta_logic(., fixed=fixed, ...))
+  #results$adjustment = ifelse(is.na(results$adjustment),"NA",results$adjustment)
+  
+  #If parallel, run the parallel version.
+  if(cores>1) {
+    if(!require(multidplyr)) {stop("Package 'multidplyr' required for parallelization. Please see https://github.com/hadley/multidplyr for details.")}
+    #Prep the cluster
+    cluster=create_cluster(cores)
+    #Iterate across all phenotype, snp, adjustment combinations.
+    out = results %>% partition(phenotype,snp,adjustment,cluster=cluster) %>% do(phewas_meta_logic(., fixed=fixed, ...)) %>% collect()
+  } else {
+    #Otherwise, just use do.
+    #Iterate across all phenotype, snp, adjustment combinations.
+    out = results %>% group_by(phenotype,snp,adjustment) %>% do(phewas_meta_logic(., fixed=fixed, ...))
+    
+  }
 }
