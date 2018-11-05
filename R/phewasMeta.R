6abc5c68084d35894418a4842da017ce70603f7a
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2018-11-05 10:45:58 -0600

    Update phewasMeta.R

--- R/phewasMeta.R
+++ R/phewasMeta.R
@@ -2,5 +2,5 @@ phewasMeta <- function(results, fixed=T, keep.both=T, ...) {
   #Replace NA adjustment values with "NA"- necessary for by
   results$adjustment = ifelse(is.na(results$adjustment),"NA",results$adjustment)
   #Iterate across all phenotype, snp, adjustment combinations.
-  out = results %>% group_by(phenotype,snp,adjustment) %>% do(phewas_meta_logic(., fixed=fixed, ...))
+  out = results %>% group_by(phenotype,snp,adjustment) %>% do(PheWAS:::phewas_meta_logic(., fixed=fixed, ...))
 }
