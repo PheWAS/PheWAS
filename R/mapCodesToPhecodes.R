baa3dd7cbc0a6562835f5539db2f7f72255241d6
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2018-11-09 17:00:42 -0600

    Update to support beta ICD10CM map

--- R/mapCodesToPhecodes.R
+++ R/mapCodesToPhecodes.R
@@ -2,21 +2,29 @@ mapCodesToPhecodes <-
   function(input, 
            vocabulary.map=PheWAS::phecode_map,
            rollup.map=PheWAS:::phecode_rollup_map,
-           make.distinct=T) {
+           make.distinct=TRUE) {
     if(sum(names(input) %in% c("vocabulary_id","code"))!=2) {
       stop("Must supply a data frame with 'vocabulary_id' and 'code' columns")
     }
     if(!class(input[["code"]]) %in% c("character","factor")) {stop("Please ensure character or factor code representation. Some vocabularies, eg ICD9CM, require strings to be represented accurately: E.G.: 250, 250.0, and 250.00 are different codes and necessitate string representation")}
-    #Perform the direct map
-    output = inner_join(input,vocabulary.map,by=c("vocabulary_id","code"))
-    #Remove old columns
-    output = output %>% mutate(vocabulary_id="phecode") %>% select(-code) %>% rename(code=phecode)
+    
+    if(!is.null(vocabulary.map)){
+      #Perform the direct map
+      output = inner_join(input,vocabulary.map,by=c("vocabulary_id","code"))
+      #Remove old columns
+      output = output %>% mutate(vocabulary_id="phecode") %>% select(-code) %>% rename(code=phecode)
+    } else {
+      #Prepare for just the phecode expansion
+      output=input
+    }
     #Make distinct
     if(make.distinct) {output = distinct(output)}
     #Perform the rollup
-    output = inner_join(output,phecode_rollup_map,by="code") %>% select(-code) %>% rename(code=phecode_unrolled)
-    #Make distinct
-    if(make.distinct) {output = distinct(output)}
+    if(!is.null(rollup.map)) {
+      output = inner_join(output,phecode_rollup_map,by="code") %>% select(-code) %>% rename(code=phecode_unrolled)
+      #Make distinct
+      if(make.distinct) {output = distinct(output)}
+    }
     
     #Return the output
     output 
