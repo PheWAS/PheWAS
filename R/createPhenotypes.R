7f8327a1fd2fd6aeea02ae07839daeedead1799a
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2019-01-08 17:20:11 -0600

    Update to support changes to mapCodesToPhecodes

--- R/createPhenotypes.R
+++ R/createPhenotypes.R
@@ -1,10 +1,10 @@
 createPhenotypes <-
   function(id.vocab.code.index, min.code.count=2, add.phecode.exclusions=T, translate=T, id.sex, 
            full.population.ids=unique(id.vocab.code.index[[1]]),
-           aggregate.fun=PheWAS:::num_distinct_dates, 
+           aggregate.fun=PheWAS::num_distinct_dates, 
            vocabulary.map=PheWAS::phecode_map,
-           rollup.map=PheWAS:::phecode_rollup_map,
-           exclusion.map=PheWAS:::phecode_exclude)
+           rollup.map=PheWAS::phecode_rollup_map,
+           exclusion.map=PheWAS::phecode_exclude)
   {
     id.name=names(id.vocab.code.index)[1]
     
@@ -18,7 +18,7 @@ createPhenotypes <-
       if(!class(id.vocab.code.index[[3]]) %in% c("character","factor")) {stop("Please ensure character or factor code representation. Some vocabularies, eg ICD9CM, require strings to be represented accurately: E.G.: 250, 250.0, and 250.00 are different codes and necessitate string representation")}
       names(id.vocab.code.index)=c("id","vocabulary_id","code","index")
       message("Mapping codes to phecodes...")
-      phemapped=mapCodesToPhecodes(id.vocab.code.index, vocabulary.map=vocabulary.map, rollup.map=rollup.map) %>% select(id, code, index)
+      phemapped=mapCodesToPhecodes(id.vocab.code.index, vocabulary.map=vocabulary.map, rollup.map=rollup.map) %>% transmute(id, code=phecode, index)
     }
     
     message("Aggregating codes...")
