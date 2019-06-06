360859e49a5b7cb08651a0406d716bb09091bb71
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2019-06-06 14:43:39 -0500

    Creation of legacy package

---
+++ R/zzz.R
@@ -0,0 +1,3 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to the legacy version of PheWAS. This version is no longer typically maintained, but is preserved for compatibility reasons. Please check https://github.com/PheWAS/PheWAS for the newest version.")
+}
