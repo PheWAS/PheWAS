888b063f6abe2015415ffd7cd39f11923c5123a0
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2020-03-11 19:28:48 -0500

    Update to numeric type identifcation
    default_code_agg() now more broadly identifies numeric types

--- R/default_code_agg.R
+++ R/default_code_agg.R
@@ -1,6 +1,6 @@
 default_code_agg <- function(index) {
   #If it's a number, sum, otherwise count distinct
-  if(max(class(index)=="numeric")==TRUE) {
+  if(is.numeric(index)==TRUE) {
     sum(index)
   } else {
     length(unique(index))
