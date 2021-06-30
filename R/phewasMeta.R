4d04a3a13d1b8c90308b330b6320f61c87c84c27
Robert Carroll <Robert.J.Carroll@vanderbilt.edu>
2021-06-30 16:41:48 -0500

    CRAN Compliance: phewasMeta
    * Included multidplyr Suggestion
    * Updated multidplyr usage
    * Removed require() for CRAN compliance. Note this will change the error behavior, removing the custom note.
    * Updated documentation

--- R/phewasMeta.R
+++ R/phewasMeta.R
@@ -1,14 +1,14 @@
 phewasMeta <- function(results, fixed=T, keep.both=T, cores=1, ...) {
   #If parallel, run the parallel version.
   if(cores>1) {
-    if(!require(multidplyr)) {stop("Package 'multidplyr' required for parallelization. Please see https://github.com/hadley/multidplyr for details.")}
     #Prep the cluster
-    cluster=create_cluster(cores)
+    cluster=multidplyr::new_cluster(cores)
     #Iterate across all phenotype, snp, adjustment combinations.
-    out = results %>% partition(phenotype,snp,adjustment,cluster=cluster) %>% do(PheWAS:::phewas_meta_logic(., fixed=fixed, ...)) %>% collect()
+    out = results %>% group_by(phenotype,snp,adjustment) %>%  multidplyr::partition(cluster) %>% do(phewas_meta_logic(., fixed=fixed, ...)) %>% collect()
   } else {
     #Otherwise, just use do.
     #Iterate across all phenotype, snp, adjustment combinations.
     out = results %>% group_by(phenotype,snp,adjustment) %>% do(phewas_meta_logic(., fixed=fixed, ...))    
   }
+  out
 }
